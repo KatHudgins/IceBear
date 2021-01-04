8e8906b362a4d5d53247d5ab4477acf85f328d96
KatHudgins <Kathryn.r.hudgins@gmail.com>
2021-01-04 16:04:19 -0600

    Initial Commit. UI & Data Layer added to Solution

---
+++ IceBear.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IceBear.UI.MVC.Startup))]
+namespace IceBear.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
